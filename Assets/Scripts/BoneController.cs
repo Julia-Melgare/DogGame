56332c261a7bae83400d20d342cf890d2ec8b353
Julia <julia.melgare@acad.pucrs.br>
2021-07-25 01:06:14 -0300

    add Unity files

---
+++ Assets/Scripts/BoneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneController : MonoBehaviour
+{
+    private Plane plane = new Plane(Vector3.up, Vector3.zero);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float enter = 0f;
+
+        if (plane.Raycast(ray, out enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            transform.position = new Vector3(hitPoint.x, hitPoint.y+2f, hitPoint.z);
+        }        
+    }
+}
---
+++ Assets/Scripts/DogController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogController : MonoBehaviour
+
+{
+    private GameObject bone;
+    public float speed = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bone = GameObject.Find("Bone");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = bone.transform.position - transform.position;
+        Debug.DrawLine(transform.position, bone.transform.position);
+        Vector3 newPos = (direction * speed) * Time.deltaTime;
+        transform.position += new Vector3(newPos.x, 0, newPos.z);
+        transform.rotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+
+    }
+}
