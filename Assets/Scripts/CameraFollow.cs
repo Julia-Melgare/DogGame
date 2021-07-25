170e1544bf196e09669b62ac8b263cc522354a66
Julia <julia.melgare@acad.pucrs.br>
2021-07-25 01:37:28 -0300

    added camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform targetObject;
+    public float smoothness;
+    private Vector3 initalOffset;
+    private Vector3 cameraPosition;
+
+    void Start()
+    {
+        initalOffset = transform.position - targetObject.position;
+    }
+
+    void Update()
+    {
+        //transform.LookAt(targetObject);
+        cameraPosition = targetObject.position + initalOffset;
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, smoothness*Time.deltaTime);
+    }
+}
