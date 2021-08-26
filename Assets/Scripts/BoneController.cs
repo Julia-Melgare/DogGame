ed8a7333351c007000372dade49cd05357eece56
Julia <julia.melgare@acad.pucrs.br>
2021-08-26 20:44:40 -0300

    changed bone relocation to move instead of tp

--- Assets/Scripts/BoneController.cs
+++ Assets/Scripts/BoneController.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class BoneController : MonoBehaviour
 {
     private Plane plane = new Plane(Vector3.up, Vector3.zero);
-    private Rigidbody rigidBody;
+    private Rigidbody rigidbody;
     private bool isGrabbed;
-    public float speed;
+    public float cursorSpeed;
+    public float rollSpeed;
+    public float fallSpeedBoost;
 
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
-        rigidBody.useGravity = false;
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
         isGrabbed = true;
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         float enter = 0f;
@@ -29,34 +31,11 @@ public class BoneController : MonoBehaviour
         {
             if (isGrabbed)
             {
-                Ray inputRay = new Ray(transform.position, Vector3.down);
-                RaycastHit hit;
-                if (Physics.Raycast(inputRay, out hit))
-                {
-                    if (LayerMask.LayerToName(hit.collider.gameObject.layer) != "Obstacles")
-                    {
-                        isGrabbed = false;
-                        rigidBody.velocity = Vector3.zero;
-                        rigidBody.useGravity = true;
-                        rigidBody.freezeRotation = false;
-                        rigidBody.constraints = RigidbodyConstraints.None;
-                    }
-                    else
-                    {
-                        Vector3 newPos = RepositionBone(hit);
-                        if (!float.IsNaN(newPos.x))
-                        {
-                            transform.position = newPos;
-                            isGrabbed = false;
-                            rigidBody.velocity = Vector3.zero;
-                            rigidBody.useGravity = true;
-                            rigidBody.freezeRotation = false;
-                            rigidBody.constraints = RigidbodyConstraints.None;
-                            
-                        }
-                    }
-
-                }
+                isGrabbed = false;
+                rigidbody.useGravity = true;
+                rigidbody.freezeRotation = false;
+                rigidbody.constraints = RigidbodyConstraints.None;
+                rigidbody.velocity = Vector3.down * fallSpeedBoost;
             }
             else
             {
@@ -66,11 +45,11 @@ public class BoneController : MonoBehaviour
                 {
                     if (LayerMask.LayerToName(hit.collider.gameObject.layer) == "Bone")
                     {
-                        rigidBody.useGravity = false;
                         isGrabbed = true;
+                        rigidbody.useGravity = false;                        
                         transform.rotation = Quaternion.Euler(90, 0, 0);
-                        rigidBody.constraints = RigidbodyConstraints.FreezePositionY;
-                        rigidBody.freezeRotation = true;
+                        rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+                        rigidbody.freezeRotation = true;
                         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                         float enter = 0f;
                         if (plane.Raycast(ray, out enter))
@@ -92,71 +71,45 @@ public class BoneController : MonoBehaviour
                 Vector3 hitPoint = ray.GetPoint(enter);
                 Vector3 direction = hitPoint - transform.position;
                 direction = direction.normalized;
-                rigidBody.velocity = speed * direction;
+                rigidbody.velocity = cursorSpeed * direction;
             }
         }
     }
 
-    bool isValidPosition(Vector3 position)
+    private void OnCollisionStay(Collision collision)
     {
-        Ray inputRay = new Ray(position, Vector3.down);
-        RaycastHit hit;
-        if (Physics.Raycast(inputRay, out hit))
+        if(!isGrabbed)
         {
-            if (LayerMask.LayerToName(hit.collider.gameObject.layer) == "Obstacles")
+            if(LayerMask.LayerToName(collision.gameObject.layer) == "Obstacles")
             {
-                return false;
+                Vector3 boneDirection = CalculateBoneDirection(collision.gameObject);
+                rigidbody.velocity = rollSpeed * boneDirection;
             }
             else
             {
-                return true;
+                rigidbody.velocity = Vector3.down * fallSpeedBoost;
             }
+            
         }
-        return false;
     }
 
-    Vector3 RepositionBone(RaycastHit hit)
+    private void OnCollisionExit(Collision collision)
     {
-        Vector3 currentPos = transform.position;
-        BoxCollider collidingObject = (BoxCollider)hit.collider;
-        float xsize = collidingObject.size.x * hit.collider.gameObject.transform.localScale.x;
-        float zsize = collidingObject.size.z * hit.collider.gameObject.transform.localScale.z;
-        Vector3[] directions = { new Vector3(-xsize, 0, 0), new Vector3(0, 0, -zsize), new Vector3(xsize, 0, 0), new Vector3(0, 0, zsize)};
-        List<Vector3> sortedDirections = new List<Vector3>(directions);
-        sortedDirections.Sort((a,b) => a.magnitude.CompareTo(b.magnitude));
-        while (directions[0].x <= xsize * 2f)
-        {
-            for (int j = 0; j < directions.Length; j++)
-            {
-                Vector3 newPos = currentPos + directions[j];
-                if (isValidPosition(newPos))
-                {
-                    return newPos;
-                }
-                else
-                {
-                    switch (j)
-                    {
-                        case 0:
-                            sortedDirections[j] += new Vector3(-2, 0, 0);
-                            break;
-                        case 1:
-                            sortedDirections[j] += new Vector3(0, 0, -2);
-                            break;
-                        case 2:
-                            sortedDirections[j] += new Vector3(2, 0, 0);
-                            break;
-                        case 3:
-                            sortedDirections[j] += new Vector3(0, 0, 2);
-                            break;
-                    }
-                }
-
-            }
-        }          
-        return new Vector3(float.NaN, float.NaN, float.NaN);
+        if (!isGrabbed && LayerMask.LayerToName(collision.gameObject.layer) == "Obstacles")
+            rigidbody.velocity = Vector3.down * fallSpeedBoost;
     }
 
-
-
+    Vector3 CalculateBoneDirection(GameObject collidingObject)
+    {
+        float xsize = collidingObject.GetComponent<Collider>().bounds.size.x;
+        float zsize = collidingObject.GetComponent<Collider>().bounds.size.z;
+        if(xsize < zsize)
+        {
+            return Vector3.left;
+        }
+        else
+        {
+            return Vector3.back;
+        }
+    }
 }
